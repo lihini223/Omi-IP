58db44428d393a8322fb3398e8ca228b366fd2c6
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-02-18 09:34:17 +0530

    Added client

---
+++ client/app.js
@@ -0,0 +1,30 @@
+const urlString = window.location.href;
+const url = new URL(urlString);
+const playerId = url.searchParams.get('playerId');
+const playerName = url.searchParams.get('playerName');
+const room = url.searchParams.get('room');
+
+const socket = io("http://localhost:3000", {
+    query: {
+        playerId,
+        playerName,
+        room
+    }
+});
+
+const table = document.querySelector('#table');
+const hand = document.querySelector('#hand');
+
+socket.on('room-full', () => {
+    console.log('Room is full');
+});
+
+socket.on('player-disconnect', () => {
+    console.log('Player disconnected');
+});
+
+socket.on('game-started', () => {
+    console.log('Game started');
+});
+
+socket.emit('card-played', 'data');
