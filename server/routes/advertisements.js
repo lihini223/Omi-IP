b275635abd1863d1f51a87e56b1d856f49277778
lihini223 <lihininisansala95@gmail.com>
2021-03-29 19:26:39 +0530

    add new adverstisement function

--- server/routes/advertisements.js
+++ server/routes/advertisements.js
@@ -27,3 +27,56 @@ const upload = multer({
     },
     storage: storage
 });
+
+
+/* router.get('/advertisements', checkAuthenticated, async (req, res) => {
+    try{
+        const advertisements = await Advertisement.find({});
+
+        res.render('advertisements', { advertisements: advertisements });
+    } catch{
+        res.render('/admins/advertisements', { advertisements: [] });
+    }
+}); */
+
+// new advertisement
+router.post('/new', checkAuthenticated, upload.single('advertisementImage'), async (req, res) => {
+    const imageName = req.file != null ? req.file.filename : null;
+
+    try{
+        const advertisements = await Advertisement.find();
+        
+        let errors = [];
+
+        if(req.body.title == '' || req.body.details == ''){
+            errors.push({ msg: 'Please enter all the details.' });
+        }
+
+        if(errors.length > 0){
+            if(imageName != null){
+                removeAdvertisementImage(imageName);
+            }
+
+            return res.render('advertisements', { advertisements, errors });
+        } else {
+            const details = req.body.details.replace(/(?:\r\n|\r|\n)/g, '. ');
+
+            const advertisement = new Advertisement({
+                title: req.body.title,
+                details: details,
+                link: req.body.link,
+                imageName
+            });
+
+            const newAdvertisement = await advertisement.save();
+
+            res.redirect('/admins/advertisements');
+        }
+    } catch(err){
+        if(imageName != null){
+            removeAdvertisementImage(imageName);
+        }
+
+        res.redirect('/admins/advertisements');
+    }
+});
